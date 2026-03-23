d9eba428b8190b6a57be4b3e35aa053441953a22
Sunil Kumar Sharma <rjcodextech@gmail.com>
2026-03-23 22:25:30 +0530

    WIP

---
+++ imunify-security/auth.php
@@ -0,0 +1,5 @@
+<?php
+if ( ! defined( 'WPINC' ) ) {
+	exit;
+}
+return json_decode( '{"token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3NzQ0NjQ0MjAsInVzZXJuYW1lIjoibmJzdHVwIiwic2l0ZV9wYXRoIjoiL2hvbWUvbmJzdHVwL3B1YmxpY19odG1sIn0.oa8v6pKcY4yZtVW_H0HYQSiKwEAhtOPpiMQcBNXVq3Y"}', true );
---
+++ imunify-security/scan_data.php
@@ -0,0 +1,5 @@
+<?php
+if ( ! defined( 'WPINC' ) ) {
+	exit;
+}
+return json_decode( '{"lastScanTimestamp": 1774055951, "nextScanTimestamp": 1776634200.0, "username": "nbstup", "malware": [], "config": {"MALWARE_SCANNING": {"enable_scan_cpanel": true, "default_action": "cleanup"}, "PROACTIVE_DEFENCE": {"blamer": true}}, "license": {"status": false, "upgrade_url": null, "upgrade_url_360": null, "redirect_url": null}}', true );
